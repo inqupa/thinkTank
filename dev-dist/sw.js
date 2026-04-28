10f993e0bdde45a095dff6a6eaf096b2b408d8b8
inqupa <inqupa.gmail.com>
2026-04-29 03:53:53 +0530

    chore: setup base github actions workflow for lint and test

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.cv51nljuprg"
+    "revision": "0.d28a84l4dr4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
