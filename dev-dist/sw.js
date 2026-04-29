5946acee6533cb18a0004029ad241c5db2546200
inqupa <inqupa.gmail.com>
2026-04-29 16:23:20 +0530

    chore: push new vite frontend bundle

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.d28a84l4dr4"
+    "revision": "0.3ollnvf0n04"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
