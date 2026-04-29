e5bc11a26f9050e7c17f2e0563181d49742fff97
inqupa <inqupa.gmail.com>
2026-04-29 15:59:26 +0530

    chore: add eslint configuration file

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        process: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn"
+    }
+  }
+];
